01986e1a9e7242dd669b9125c9286ad3c649f71b
Александр <yudenichaa@yandex.ru>
2020-03-23 13:38:04 +0300

    3

--- src/components/Places.js
+++ src/components/Places.js
@@ -26,28 +26,6 @@ class Places extends React.Component {
         };
     }
 
-    createSector = (rows, cols) => {
-        return Array(rows).fill().map(() => Array(cols).fill());
-    }
-
-    showModalWindow = (title, text) => {
-        this.setState({
-            modalWindow: <ModalWindow
-                title={title}
-                text={text}
-                handleCloseClicked={this.handleModalWindowClose} />
-        });
-    }
-
-    handleModalWindowClose = () => this.setState({ modalWindow: null });
-
-    checkIsNumberOrEmpty = (value) => {
-        if (value == "") return "";
-        const number = Number(value);
-        if (number && number >= 1) return number;
-        return NaN;
-    }
-
     handleNameChange = e => this.setState({ name: e.target.value });
 
     handleSectorChange = e => {
@@ -57,66 +35,133 @@ class Places extends React.Component {
 
     handleRowChange = e => {
         const value = this.checkIsNumberOrEmpty(e.target.value);
-        if (!isNaN(value)) this.setState({ sector: value });
+        if (!isNaN(value)) this.setState({ row: value });
     }
 
     handlePlaceChange = e => {
         const value = this.checkIsNumberOrEmpty(e.target.value);
-        if (!isNaN(value)) this.setState({ sector: value });
+        if (!isNaN(value)) this.setState({ place: value });
     }
 
     handleAddButtonClicked = () => {
-        if (this.state.name.length == 0) {
-            this.showModalWindow("Распределение мест", "Введите фамилию");
-            return;
-        }
-        if (this.state.sector.length == 0) {
-            this.showModalWindow("Распределение мест", "Введите номер сектора");
-            return;
-        }
-        if (this.state.sector > 3) {
-            this.showModalWindow("Распределение мест", "Номер сектора должен быть в диапазоне от 1 до 3 включительно");
-            return;
-        }
-        if (this.state.row.length == 0) {
-            this.showModalWindow("Распределение мест", "Введите номер ряда");
-            return;
-        }
-        if (this.state.row > this.state.sectorSizes[this.state.sector].rows) {
-            this.showModalWindow("Распределение мест", "Номер ряда в секторе " + this.state.sector + " должен быть в диапазоне от 1 до " + this.state.sectorSizes[this.state.sector].rows + " включительно");
-            return;
-        }
-        if (this.state.place.length == 0) {
-            this.showModalWindow("Распределение мест", "Введите номер места");
-            return;
-        }
-        this.setState(state => ({
-            mapNamesToPlaces: {
-                [state.name]: {
-                    "sector": state.sector,
-                    "row": state.row,
-                    "place": state.place
+        if (this.checkName() && this.checkSector &&
+            this.checkRow() && this.checkPlace() &&
+            this.checkPlaceTaken()) {
+            this.setState(state => ({
+                mapNamesToPlaces: {
+                    [state.name]: {
+                        "sector": state.sector - 1,
+                        "row": state.row - 1,
+                        "place": state.place - 1
+                    },
+                    ...state.mapNamesToPlaces
                 },
-                ...state.mapNamesToPlaces
-            }
-        }));
+                sectors: state.sectors.map((sector, index) => {
+                    if (index == state.sector - 1)
+                        return sector.map((row, index) => {
+                            if (index == state.row - 1)
+                                return row.map((name, index) => {
+                                    if (index == state.place - 1)
+                                        return state.name;
+                                    else return name;
+                                });
+                            else return row;
+                        })
+                    else return sector;
+                })
+            }), () => console.log(this.state.mapNamesToPlaces, this.state.sectors));
+        }
     };
+
+    handleRemovePerson = () => {
+        if (this.checkName() && this.checkPersonExists()) {
+            this.setState(state => ({
+                mapNamesToPlaces: Object.keys(state.mapNamesToPlaces)
+                    .reduce((result, key) => {
+                        if (key !== state.name)
+                            result[key] = state.mapNamesToPlaces[key];
+                        return result;
+                    }, {}),
+                sectors: state.sectors.map(sector => {
+                    return sector.map(row => {
+                        return row.map(name => {
+                            if (name == state.name)
+                                return undefined;
+                            else return name;
+                        })
+                    })
+                })
+            }), () => console.log(this.state.mapNamesToPlaces, this.state.sectors));
+        }
+    }
+
+    handleClearPlace = () => {
+        if (this.checkSector() && this.checkRow() && this.checkPlace() && this.checkPlaceEmpty()) {
+            this.setState(state => ({
+                mapNamesToPlaces: Object.keys(state.mapNamesToPlaces)
+                    .reduce((result, name) => {
+                        if (state.mapNamesToPlaces[name].sector != state.sector - 1 ||
+                            state.mapNamesToPlaces[name].row != state.row - 1 ||
+                            state.mapNamesToPlaces[name].place != state.place - 1)
+                            result[name] = state.mapNamesToPlaces[name];
+                        return result;
+                    }, {}),
+                sectors: state.sectors.map((sector, index) => {
+                    if (index == state.sector - 1)
+                        return sector.map((row, index) => {
+                            if (index == state.row - 1)
+                                return row.map((name, index) => {
+                                    if (index == state.place - 1)
+                                        return undefined;
+                                    else return name;
+                                });
+                            else return row;
+                        })
+                    else return sector;
+                })
+            }), () => console.log(this.state.mapNamesToPlaces, this.state.sectors));
+        }
+    }
+
+    handleReplaceButtonClicked = () => {
+        if (this.checkName() && this.checkSector &&
+            this.checkRow() && this.checkPlace() &&
+            this.checkPlaceEmpty()) {
+            this.setState(state => ({
+                mapNamesToPlaces: Object.keys(state.mapNamesToPlaces)
+                    .reduce((result, name) => {
+                        if (state.mapNamesToPlaces[name].sector == state.sector - 1 &&
+                            state.mapNamesToPlaces[name].row == state.row - 1 &&
+                            state.mapNamesToPlaces[name].place == state.place - 1)
+                            result[state.name] = state.mapNamesToPlaces[name];
+                        else
+                            result[name] = state.mapNamesToPlaces[name];
+                        return result;
+                    }, {}),
+                sectors: state.sectors.map((sector, index) => {
+                    if (index == state.sector - 1)
+                        return sector.map((row, index) => {
+                            if (index == state.row - 1)
+                                return row.map((name, index) => {
+                                    if (index == state.place - 1)
+                                        return state.name;
+                                    else return name;
+                                });
+                            else return row;
+                        })
+                    else return sector;
+                })
+            }), () => console.log(this.state.mapNamesToPlaces, this.state.sectors));
+        }
+    }
 
     handleResetButtonClicked = () => {
         this.setState({
-            name: null,
-            sector: null,
-            row: null,
-            place: null
+            name: "",
+            sector: "",
+            row: "",
+            place: ""
         });
-    }
-
-    handleDeleteButtonClicked = () => {
-        alert("Delete");
-    }
-
-    handeReplaceButtonClicked = () => {
-        alert("Replace");
     }
 
     handleSearchNameChanged = e => this.setState({ searchName: e.target.value });
@@ -130,7 +175,7 @@ class Places extends React.Component {
             <React.Fragment>
                 {this.state.modalWindow}
                 <div className="row justify-content-center">
-                    <div className="col-4 border rounded p-2 mt-2 mr-2">
+                    <div className="col-6 border rounded p-2 mt-2 mr-2">
                         <form>
                             <h4 className="text-center">Распределение мест</h4>
                             <div className="form-row mt-2">
@@ -178,19 +223,25 @@ class Places extends React.Component {
                                         type="button"
                                         onClick={this.handleAddButtonClicked}>
                                         Добавить
-                                </button>
+                                    </button>
                                     <button
                                         className="btn btn-primary btn-sm mr-2"
                                         type="button"
-                                        onClick={this.handleDeleteButtonClicked}>
-                                        Удалить
-                                </button>
+                                        onClick={this.handleRemovePerson}>
+                                        Удалить участника
+                                    </button>
                                     <button
                                         className="btn btn-primary btn-sm mr-2"
                                         type="button"
-                                        onClick={this.handeReplaceButtonClicked}>
+                                        onClick={this.handleClearPlace}>
+                                        Освободить место
+                                    </button>
+                                    <button
+                                        className="btn btn-primary btn-sm mr-2"
+                                        type="button"
+                                        onClick={this.handleReplaceButtonClicked}>
                                         Заменить
-                                </button>
+                                    </button>
                                     <button
                                         className="btn btn-primary btn-sm"
                                         type="reset"
@@ -227,6 +278,114 @@ class Places extends React.Component {
                 </div>
             </React.Fragment>
         );
+    }
+
+    checkName = () => {
+        const name = this.state.name;
+        if (name.length == 0) {
+            this.showModalWindow("Распределение мест", "Введите фамилию");
+            return false;
+        }
+        return true;
+    }
+
+    checkSector = () => {
+        const sector = this.state.sector;
+        if (sector.length == 0) {
+            this.showModalWindow("Распределение мест", "Введите номер сектора");
+            return false;
+        }
+        if (sector > 3) {
+            this.showModalWindow("Распределение мест",
+                "Номер сектора должен быть в диапазоне от 1 до 3 включительно");
+            return false;
+        }
+        return true;
+    }
+
+    checkRow = () => {
+        const row = this.state.row;
+        const sector = this.state.sector;
+        if (row.length == 0) {
+            this.showModalWindow("Распределение мест", "Введите номер ряда");
+            return false;
+        }
+        if (row > this.state.sectorSizes[sector].rows) {
+            this.showModalWindow("Распределение мест",
+                `Номер ряда в секторе ${sector} должен быть в диапазоне от 1
+                 до ${this.state.sectorSizes[sector].rows} включительно`);
+            return false;
+        }
+        return true;
+    }
+
+    checkPlace = () => {
+        const sector = this.state.sector;
+        const place = this.state.place;
+        if (place.length == 0) {
+            this.showModalWindow("Распределение мест", "Введите номер места");
+            return false;
+        }
+        if (place > this.state.sectorSizes[sector].cols) {
+            this.showModalWindow("Распределение мест",
+                `Номер места в секторе ${sector} должен быть в диапазоне от 1
+                 до ${this.state.sectorSizes[sector].cols} включительно`);
+            return false;
+        }
+        return true;
+    }
+
+    checkPlaceTaken = () => {
+        const sector = this.state.sector;
+        const row = this.state.row;
+        const place = this.state.place;
+        if (this.state.sectors[sector - 1][row - 1][place - 1] !== undefined) {
+            this.showModalWindow("Распределение мест",
+                `Узазанное место занято: ${this.state.sectors[sector - 1][row - 1][place - 1]}`);
+            return false;
+        }
+        return true;
+    }
+
+    checkPlaceEmpty = () => {
+        const sector = this.state.sector;
+        const row = this.state.row;
+        const place = this.state.place;
+        if (this.state.sectors[sector - 1][row - 1][place - 1] === undefined) {
+            this.showModalWindow("Распределение мест", "Узазанное место свободно");
+            return false;
+        }
+        return true;
+    }
+
+    checkPersonExists = () => {
+        if (this.state.mapNamesToPlaces[this.state.name] === undefined) {
+            this.showModalWindow("Распределение мест", "Указанного участника не существует");
+            return;
+        }
+        return true;
+    }
+
+    checkIsNumberOrEmpty = (value) => {
+        if (value == "") return "";
+        const number = Number(value);
+        if (number && number >= 1) return number;
+        return NaN;
+    }
+
+    showModalWindow = (title, text) => {
+        this.setState({
+            modalWindow: <ModalWindow
+                title={title}
+                text={text}
+                handleCloseClicked={this.handleModalWindowClose} />
+        });
+    }
+
+    handleModalWindowClose = () => this.setState({ modalWindow: null });
+
+    createSector = (rows, cols) => {
+        return Array(rows).fill().map(() => Array(cols).fill());
     }
 }
 
